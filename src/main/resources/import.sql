823cf3ac9335070df46f9910afe3b0278e306260
TAmyG <tamy.vivas@gmail.com>
2018-12-11 22:50:28 -0600

    feat: Gestion de errores para controller
    Metodo create y update, implementacion de @valid y Binding para parametro Cliente
    Manejo de stream para convertir errorField a lista String

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Tamy' , 'Vivas', 'tamy.vivas@gmail.com', '2018-11-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Juan' , 'Lopez', 'juan@gmail.com', '2018-11-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Carlos' , 'Perez', 'carlos@gmail.com', '2018-10-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Manuel' , 'Agustin', 'manuel@gmail.com', '2018-12-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Elder' , 'Coman', 'elder@gmail.com', '2018-09-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Josue' , 'Pop', 'josue@gmail.com', '2018-08-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('John' , 'Doe', 'john@gmail.com', '2018-07-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Luis' , 'Estrada', 'luis@gmail.com', '2018-06-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Esvin' , 'Sac', 'esvin@gmail.com', '2018-05-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Moy' , 'Zepeda', 'moy@gmail.com', '2018-04-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Bart' , 'Simpson', 'bart@gmail.com', '2018-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('XD' , 'Meme', 'xd@gmail.com', '2018-02-02');
